d5d9917ec5a5d45662063da1c6424c18871f125a
Jake Miller <hillsighed@gmail.com>
2016-12-07 15:18:00 -0800

    Removes expenses list and all associated js. Re-organizes code

--- main.js
+++ main.js
@@ -1,8 +1,6 @@
 console.log('linked!')
 
-// The UL that contains all the expenses
-var expenseList = document.querySelector('.expenses');
-
+var table = document.getElementById('expensesTable');
 
 // Expense Name and amount
 var expenseAmInput = document.getElementById('expenseAmountInput');
@@ -11,7 +9,7 @@ var expenseNmInput = document.getElementById('expenseNameInput');
 // Current Expenses
 totalExpenses = 0;
 var countExpenses = function() {
-  totalExpenses = document.querySelectorAll('li').length;
+  totalExpenses = document.querySelectorAll('tr').length - 1;
   document.getElementById('currentTotalExpenses').innerHTML = totalExpenses;
 };
 
@@ -21,26 +19,9 @@ var addExpense = function (){
   if (expenseAmInput.value == "" || expenseNmInput.value == "") {
     return false;
   };
-
-  // Add Expense Name and cost to Expenses list
-  var newExpense = document.createElement('li');
-    newExpense.classList.add('expense');
-    //newExpense.innerHTML = "Name: " + expenseNmInput.value + " Amount: " + expenseAmInput.value + "  ";
-  var expenseNameSpan = document.createElement('span');
-    expenseNameSpan.classList.add('expenseName');
-    expenseNameSpan.innerHTML = expenseNmInput.value;
-  var expenseAmountSpan = document.createElement('span');
-    expenseAmountSpan.classList.add('expenseAmount');
-    expenseAmountSpan.innerHTML = expenseAmInput.value;
-  var remSpan = document.createElement('span');
-    remSpan.innerHTML = "X";
-    remSpan.classList.add('remove');
-  newExpense.appendChild(expenseNameSpan);
-  newExpense.appendChild(expenseAmountSpan);
-  newExpense.appendChild(remSpan);
-  expenseList.appendChild(newExpense);
-  countExpenses();
   addExpensesToTable();
+  countExpenses();
+
 };
 
 
@@ -48,45 +29,8 @@ var addExpense = function (){
 var button = document.querySelector('button');
 button.addEventListener('click', addExpense);
 
-// Add Event Listener to UL to perform task removal
-expenseList.addEventListener('click', function (evt){
-  if (evt.target.classList.contains('remove')) {
-      event.target.parentNode.remove();
-      countExpenses();
-      return true;
-  }
-});
 
 
-// Sort Expenses based off of Amount from highest to lowest
-// add event listener to sort button
-
-var sortButton = document.querySelector('#sort-button');
-sortButton.addEventListener('click', hope);
-var hope = function () {
-  var list = document.querySelector('.expenses');
-
-  var items = list.childNodes;
-  var itemsArr = [];
-  for (var i in items) {
-    if (items[i].nodeType == 1) { // get rid of the whitespace text nodes
-        itemsArr.push(items[i]);
-    }
-  }
-
-  itemsArr.sort(function(a, b) {
-    return a.childNodes[1].innerHTML == b.childNodes[1].innerHTML
-          ? 0
-          : (a.childNodes[1].innerHTML < b.childNodes[1].innerHTML ? -1 : 1);
-  });
-
-  for (i = 0; i < itemsArr.length; ++i) {
-    list.appendChild(itemsArr[i]);
-  }
-};
-
-
-var table = document.getElementById('expensesTable');
 // function for adding expenses to a table
 var addExpensesToTable = function () {
 var row = table.insertRow(table.rows.length);
@@ -106,8 +50,9 @@ table.addEventListener('click', function (evt){
     console.log(evt);
     console.log(evt.target.parentNode.parentNode);
     evt.target.parentNode.parentNode.remove();
+    countExpenses();
+    return true;
   };
-
 });
 
 
