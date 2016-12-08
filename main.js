8f79b6c6cd80ef7a02d5663060a2016f5fdb1449
Abi Munoz <abimunozr@gmail.com>
2016-12-07 18:06:18 -0800

    adds comments

--- main.js
+++ main.js
@@ -6,35 +6,46 @@ var expenseAmount = document.querySelector('.expense-amount');
 
 // adds expense name
 var add = function(event){
+
+  // create element tr and td
   var tr = document.createElement('tr');
   var td1 = document.createElement('td');
   var td2 = document.createElement('td');
-  var td3 = document.createElement('td');
+
+  // append td --> tr
   tr.appendChild(td1);
   tr.appendChild(td2);
-  tr.appendChild(td3);
 
+  // get input value from expense name
   var inputEN = expenseName.value;
   td1.textContent = inputEN;
 
+  // get input value from expense amount
   var inputA = expenseAmount.value;
   td2.textContent = inputA;
 
-   // add x
-  var span = document.createElement('span');
-  span.classList.add('remove');
-  span.textContent = 'x';
-  td3.appendChild(span);
+  // add x
+  var td3 = document.createElement('td');
+  td3.classList.add('remove');
+  td3.textContent = 'x';
+  tr.appendChild(td3);
 
-  span.addEventListener('click', remove);
+  // add event listener for removing x
+  td3.addEventListener('click', remove);
 
+  // append tr to the table
   var table = document.querySelector('.expenses');
   table.appendChild(tr);
 
 };
 
+// add event listener for button click
 button.addEventListener('click', add);
 
+// add function to remove
 var remove = function(event){
-  this.parentNode.remove();
+    this.parentNode.remove();
   }
+
+
+
