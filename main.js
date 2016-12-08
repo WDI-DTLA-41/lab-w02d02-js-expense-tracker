6cea1cadb0f5e71cb2d735ee8d35c2b21478f23b
Barrett Quan <21barrett@gmail.com>
2016-12-07 17:07:56 -0800

    adding data works, total tracker wip

--- main.js
+++ main.js
@@ -6,35 +6,36 @@ var input = document.querySelector('form');
 var button = document.querySelector('#add-expense');
 var formName = document.querySelector('#formName');
 var formAmount = document.querySelector('#formAmount');
+var table = document.querySelector('tbody')
+var numRows = document.querySelectorAll('tr').length;//includes one extra row
 
 var handleInput = function (event) {
-  formName.textContent = event.target.value;
-
   if (formName.value === '' || formAmount.value === '') {
     console.log('missing info')
-    } else console.log('goodjob');
+    } else {
+      console.log('Name:' + formName.value);
+      console.log('Amount:' + formAmount.value);
+      var tr = document.createElement('tr'); //creates new row
+      tr.innerHTML = "<td class ='inputData'>" + formName.value + "</td><td class='cost'>" + formAmount.value + "</td>"
+      table.appendChild(tr);
+      //clears forms
+      formName.value = '';
+      formAmount.value ='';
+      calcTotal();
+    }
 
   console.log('button was clicked', event.target);
-    var td = document.createElement('td');
-    var tr = document.createElement('tr');
+
 
 }
 button.addEventListener('click', handleInput);
-// input.addEventListener('keypress', handleInput);
-
-//to do after adding event listener from inputs
-// var createRow = function () {
-//   var td = document.createElement('td');
-//   var tr = document.createElement('tr');
-
-// }
-
 
 //to do after adding event listener of adding new tables and rows
-// var calcTotal = function (){//returns total expenses of all td's
-//   var total;
-//   for (var i = 0; 0 < cost.length; i++) {
-//   total += cost[i].textContent;
-// }
-// }
+var calcTotal = function (){
+  var runningTotal;
+  for (var i = 0; 0 < numRows.length - 1; i++) {
+  runningTotal += cost[i].textContent;
+}
+total.innerHTML = runningTotal;
+}
 
