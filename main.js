94311fb01ac584d7949fe1fe45b52284f1a612e2
Lacey Madison <laceyannmadison@gmail.com>
2016-12-07 16:33:07 -0800

    counter works

--- main.js
+++ main.js
@@ -6,14 +6,30 @@ var button = document.querySelector('#add-expense');
 var expName = document.querySelector('#name');
 var amount = document.querySelector('#amount');
 
+var counter = function() {
+  var expTable = document.querySelector('tbody');
+  var total = document.querySelector('.expense-total');
+  amtSum = 0;
+  for (i=2; i<=expTable.children.length; i++) {
+      var addThis = document.querySelector('.amt').innerText;
+      amtSum = amtSum + parseInt(addThis);
+      totalNum = parseInt(amtSum);
+      total.innerHTML= totalNum;
+  }
+}
+
 var addExp = function(evt) {
   console.log('clicked');
   evt.preventDefault();
   var newExp = document.createElement('tr');
   var expTable = document.querySelector('tbody');
-  newExp.innerHTML = '<td id="exp">' + expName.value + '</td><td id="amt">' + amount.value + '</td>';
+  newExp.innerHTML = '<td class="exp">' + expName.value + '</td><td class="amt">' + amount.value + '</td>';
   expTable.appendChild(newExp);
+  counter();
 }
 
+
+
 button.addEventListener('click', addExp)
+
 
