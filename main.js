5dab33e2daa36da97e0fbbbe83347235e42625ab
Lacey Madison <laceyannmadison@gmail.com>
2016-12-08 10:41:47 -0800

    list and counter work

--- main.js
+++ main.js
@@ -1,10 +1,9 @@
 var button = document.querySelector('#add-expense');
-
-//when button is clicked
-//add a new tr with two children- amount and name
-
 var expName = document.querySelector('#name');
 var amount = document.querySelector('#amount');
+
+
+//display the sum of all expense amounts
 
 var counter = function() {
   var expTable = document.querySelector('tbody');
@@ -18,13 +17,28 @@ var counter = function() {
   }
 }
 
+// add items to table
+
 var addExp = function(evt) {
   console.log('clicked');
   evt.preventDefault();
   var newExp = document.createElement('tr');
   var expTable = document.querySelector('tbody');
-  newExp.innerHTML = '<td class="exp">' + expName.value + '</td><td class="amt">' + amount.value + '</td>';
+  newExp.innerHTML = '<td class="exp">' + expName.value + '</td><td class="amt">' + amount.value + '</td>' + remove;
   expTable.appendChild(newExp);
+  // remove items from table
+  var remove = document.createElement('button');
+  remove.textContent = 'Remove';
+  newExp.appendChild(remove);
+  var removeExp = function() {
+  console.log('clicked');
+  this.parentNode.remove()
+}
+  // add eventlistener to x
+  for (var i = 0; i <= remove.length; i++) {
+    remove[i].addEventListener('click', removeExp);
+  }
+  // run counter
   counter();
 }
 
