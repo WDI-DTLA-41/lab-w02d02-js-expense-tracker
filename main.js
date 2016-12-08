ad98202f70469d2a4545c409f7f69732c96b8fc9
Kristopher Parrillo <kkparrillo@gmail.com>
2016-12-07 22:12:05 -0800

    delete button

--- main.js
+++ main.js
@@ -1,103 +1,41 @@
 console.log('Let it snow, let it snow, let it snow!');
 
+var button = document.querySelector('#add-expense');
+var expenseTable = document.querySelector('tbody');
+var inputExpenseName = document.querySelector('#expense-name');
+var inputExpenseAmount = document.querySelector('#expense-amount');
 
-// create div class 'container-expName' and 'container-expAmount'
-var containerExpName = document.createElement('div');
-var containerExpAmount = document.createElement('div');
-containerExpName.className = 'container-expName';
-containerExpAmount.className = 'container-expAmount';
-document.body.appendChild(containerExpName);
-document.body.appendChild(containerExpAmount);
+// create table headers
 
-// create input field 'inputExpenseName'
-var inputExpenseName = document.createElement('input');
-inputExpenseName.type = 'text';
-inputExpenseName.className = 'expName';
-inputExpenseName.setAttribute('placeholder','Expense Name');
+var handleInput = function(event) {
+// things to do when clicked
 
-// create input field 'inputExpenseAmount'
-var inputExpenseAmount = document.createElement('input');
-inputExpenseAmount.type = 'text';
-inputExpenseAmount.className = 'expAmount';
-inputExpenseAmount.setAttribute('placeholder', 'Expense Amount');
+  // create Table Row
+  var expenseRow = document.createElement('tr');
 
-// append input fields to body
-containerExpName.appendChild(inputExpenseName);
-containerExpAmount.appendChild(inputExpenseAmount);
+  // create 3 table Data
+  var expenseNameData = document.createElement('td');
+  var expenseAmountData = document.createElement('td');
+  var expenseDeleteData = document.createElement('td');
 
-// create a button called 'Add Expense'
-var addExpenseButton = document.createElement('button');
-addExpenseButton.textContent = "Add Expense";
-document.body.appendChild(addExpenseButton);
+  // add input values to Data
+  expenseNameData.textContent = inputExpenseName.value;
+  expenseAmountData.textContent = inputExpenseAmount.value;
+  expenseTable.appendChild(expenseRow);
+  expenseRow.appendChild(expenseNameData);
+  expenseRow.appendChild(expenseAmountData);
+  expenseAmountData.classList.add('amount');
 
-// create Total Expense Calculator
-var totalExpenseContainer = document.createElement('div');
-totalExpenseContainer.className = "totalCalcu";
-var totalExpensePTag = document.createElement('p');
-totalExpensePTag.textContent = "Total Expense ";
-totalExpenseContainer.appendChild(totalExpensePTag);
-document.body.appendChild(totalExpenseContainer);
-
-// create a Table called 'expenseTable'
-var expenseTable = document.createElement('table');
-expenseTable.className = 'expenseTable';
-document.body.appendChild(expenseTable);
-
-// create a Table Row
-var tableRow = document.createElement('TR');
-expenseTable.appendChild(tableRow);
-
-// create three Table Headers -- ExpName and ExpAmount and tableHeaderDelete
-var tableHeaderExpName = document.createElement('TH');
-var tableHeaderExpAmount = document.createElement('TH');
-var tableHeaderDelete = document.createElement('TH');
-tableHeaderExpName.textContent = "Name";
-tableHeaderExpAmount.textContent = "Amount";
-tableHeaderDelete.textContent = 'Delete';
-tableRow.appendChild(tableHeaderExpName);
-tableRow.appendChild(tableHeaderExpAmount);
-tableRow.appendChild(tableHeaderDelete);
-
-
-// create function handler to create Table Data
-var handleCreate = function(event) {
-  console.log(inputExpenseAmount.value,inputExpenseName.value);
-  var tableDataExpName = document.createElement('TD');
-  var tableDataExpAmount = document.createElement('TD');
-  var tableDataDelete = document.createElement('TD');
-  var tableDataDeleteButton = document.createElement('button');
-  tableDataDeleteButton.className = 'Delete';
-  tableDataDeleteButton.textContent = 'Delete';
-  tableDataExpName.textContent = inputExpenseName.value;
-  tableDataExpAmount.textContent = inputExpenseAmount.value;
-  tableDataExpName.className = 'ExpenseName';
-  tableDataExpAmount.className = 'ExpenseAmount';
-  tableHeaderExpName.appendChild(tableDataExpName);
-  tableHeaderExpAmount.appendChild(tableDataExpAmount);
-  tableHeaderDelete.appendChild(tableDataDeleteButton);
-
+  // create delete button
+  var deleteButton = document.createElement('button');
+  deleteButton.innerHTML = 'Delete';
+  expenseDeleteData.appendChild(deleteButton);
+  expenseRow.appendChild(expenseDeleteData);
 };
 
-var sumTotal = function() {
-
-  return input.value;
-}
-
-// create function handler to delete Data
 var handleDelete = function(event) {
-  var allName = document.querySelectorAll('.ExpanseName');
-  var allAmount = document.querySelectorAll('.ExpenseAmount');
-  var allDelete = document.querySelectorAll('.Delete');
-  console.log();
+  event.target.parentNode.parentNode.remove(event.target);
 }
 
-
-addExpenseButton.addEventListener('click', handleCreate);
-
+button.addEventListener('click', handleInput);
 expenseTable.addEventListener('click', handleDelete);
-
-
-
-
-
-
