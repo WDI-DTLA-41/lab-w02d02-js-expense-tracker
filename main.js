4db1da75fcd10dc009c25620fd31540ed834282d
Abi Munoz <abimunozr@gmail.com>
2016-12-07 17:24:37 -0800

    removes only x

--- main.js
+++ main.js
@@ -4,13 +4,29 @@ var button = document.querySelector('button');
 var expenseName = document.querySelector('.expense-name');
 var expenseAmount = document.querySelector('.expense-amount');
 
+// adds expense name
 var add = function(event){
   var tr = document.createElement('tr');
-  var td = document.createElement('td');
-  tr.appendChild(td);
+  var td1 = document.createElement('td');
+  var td2 = document.createElement('td');
+  var td3 = document.createElement('td');
+  tr.appendChild(td1);
+  tr.appendChild(td2);
+  tr.appendChild(td3);
 
   var inputEN = expenseName.value;
-  td.textContent = inputEN;
+  td1.textContent = inputEN;
+
+  var inputA = expenseAmount.value;
+  td2.textContent = inputA;
+
+   // add x
+  var span = document.createElement('span');
+  span.classList.add('remove');
+  span.textContent = 'x';
+  td3.appendChild(span);
+
+  span.addEventListener('click', remove);
 
   var table = document.querySelector('.expenses');
   table.appendChild(tr);
@@ -19,3 +35,6 @@ var add = function(event){
 
 button.addEventListener('click', add);
 
+var remove = function(event){
+  this.parentNode.remove();
+  }
