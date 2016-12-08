4f80e8a70ec389e1896f24b6b8ccab7f4a15e779
Barrett Quan <21barrett@gmail.com>
2016-12-07 19:01:16 -0800

    expense report reduces as intended

--- main.js
+++ main.js
@@ -1,41 +1,55 @@
 console.log('hi')
 
 var total = document.querySelector('.expense-total');
-var cost = document.querySelectorAll('.cost');
 var input = document.querySelector('form');
 var button = document.querySelector('#add-expense');
 var formName = document.querySelector('#formName');
 var formAmount = document.querySelector('#formAmount');
 var table = document.querySelector('tbody')
 var numRows = document.querySelectorAll('tr').length;//includes one extra row
+var report = [];
+ var runningTotal = [];
+   var newTotal;
+   var digit = 0;
+
+
 
 var handleInput = function (event) {
   if (formName.value === '' || formAmount.value === '') {
     console.log('missing info')
     } else {
-      console.log('Name:' + formName.value);
-      console.log('Amount:' + formAmount.value);
+      // console.log('Name:' + formName.value);
+      // console.log('Amount:' + formAmount.value);
       var tr = document.createElement('tr'); //creates new row
       tr.innerHTML = "<td class ='inputData'>" + formName.value + "</td><td class='cost'>" + formAmount.value + "</td>"
       table.appendChild(tr);
+      report.push(formAmount.value);
       //clears forms
       formName.value = '';
       formAmount.value ='';
+      runningTotal = [];
       calcTotal();
     }
 
-  console.log('button was clicked', event.target);
-
+  // console.log('button was clicked', event.target);
 
 }
 button.addEventListener('click', handleInput);
 
 //to do after adding event listener of adding new tables and rows
 var calcTotal = function (){
-  var runningTotal;
-  for (var i = 0; 0 < numRows.length - 1; i++) {
-  runningTotal += cost[i].textContent;
+
+for (var i = 0; i < report.length; i++) {
+
+   digit = (parseFloat(report[i]));
+   runningTotal.push(digit);
+   console.log('digit = ', digit);
+   console.log(runningTotal);
+    }
+  newTotal = runningTotal.reduce(function(x, y) {
+       return x + y})
+
+  total.innerText = newTotal;
 }
-total.innerHTML = runningTotal;
-}
+
 
