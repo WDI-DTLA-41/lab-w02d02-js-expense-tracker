4a3774e4b7aa8da8bc18f63a5bd7b2d21b3cde76
Lacey Madison <laceyannmadison@gmail.com>
2016-12-10 21:20:30 -0800

    ok this is the good one don't lose it

--- main.js
+++ main.js
@@ -1,47 +1,3 @@
-var button = document.querySelector('#add-expense');
-var expName = document.querySelector('#name');
-var amount = document.querySelector('#amount');
-
-
-//display the sum of all expense amounts
-
-var counter = function() {
-  var expTable = document.querySelector('tbody');
-  var total = document.querySelector('.expense-total');
-  amtSum = 0;
-  for (i=2; i<=expTable.children.length; i++) {
-      var addThis = document.querySelector('.amt').innerText;
-      amtSum = amtSum + parseInt(addThis);
-      totalNum = parseInt(amtSum);
-      total.innerHTML= totalNum;
-  }
-}
-
-// add items to table
-
-var addExp = function(evt) {
-  console.log('clicked');
-  evt.preventDefault();
-  var newExp = document.createElement('tr');
-  var expTable = document.querySelector('tbody');
-  newExp.innerHTML = '<td class="exp">' + expName.value + '</td><td class="amt">' + amount.value + '</td>' + remove;
-  expTable.appendChild(newExp);
-  // remove items from table
-  var remove = document.createElement('button');
-  remove.textContent = 'Remove';
-  newExp.appendChild(remove);
-  var removeExp = function() {
-  console.log('clicked');
-  this.parentNode.remove()
-}
-  // add eventlistener to x
-  for (var i = 0; i <= remove.length; i++) {
-    remove[i].addEventListener('click', removeExp);
-  }
-  // run counter
-  counter();
-console.log('hello');
-
 var tbody = document.querySelector('tbody');
 var btn = document.querySelector('button');
 var expName = document.querySelector('input[name="exp-name"]');
@@ -107,15 +63,6 @@ var handleRemove = function() {
   subtractTotal(subtract);
 }
 
-
-
-button.addEventListener('click', addExp)
-
-btn.addEventListener('click', handleClick);
-
-
-
-
 var getValues = function() {
   var val = parseFloat(expAmount.value) || 0
   return {
@@ -128,5 +75,9 @@ var clearInputs = function() {
   expAmount.value = '';
   expName.value = '';
 }
+
+
+btn.addEventListener('click', handleClick);
+
 
 
