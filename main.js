c4b81a63453364fe6482c09c3f07f2f7fbc4eab5
Phil Lamplugh <phillip.lamplugh@gmail.com>
2016-12-29 14:26:38 -0800

    update solution

--- main.js
+++ main.js
@@ -1,24 +1,23 @@
-var btn = document.querySelector('#add-expense');
-var expAmount = document.querySelector('input[name="expense-amount"]');
-var expName = document.querySelector('input[name="expense-name"]');
-var expTotal = document.querySelector('.expense-total');
-var expTable = document.querySelector('.expenses tbody');
+var $addExpenseBtn = document.querySelector('#add-expense');
+var $expAmount = document.querySelector('input[name="expense-amount"]');
+var $expName = document.querySelector('input[name="expense-name"]');
+var $expTotal = document.querySelector('.expense-total');
+var $expTable = document.querySelector('.expenses tbody');
+var $headingName = document.querySelector('.heading-name');
+var $headingAmount = document.querySelector('.heading-amount');
 
 var total = 0;
 
 var hasValidValues = function() {
-  return expName.value !== '' & expAmount.value !== '';
+  return $expName.value !== '' & $expAmount.value !== '';
 }
 
 var getExpenseValues = function(){
+  var amount = parseFloat($expAmount.value) || 0;
   return {
-    name: expName.value,
-    amount: parseFloat(expAmount.value) || 0
+    name: $expName.value,
+    amount: amount
   }
-}
-
-var renderTotal = function() {
-  expTotal.textContent = total;
 }
 
 var renderRow = function(exp) {
@@ -26,29 +25,62 @@ var renderRow = function(exp) {
 }
 
 var clearExpenseValues = function() {
-  expAmount.value = '';
-  expName.value = '';
+  $expAmount.value = '';
+  $expName.value = '';
 }
 
-var handleClick = function(evt) {
+var addExpense = function(evt) {
   evt.preventDefault();
-  if ( !hasValidValues() ) return false;
+  if ( !hasValidValues() ) {
+    return false;
+  }
   var expense = getExpenseValues();
-  expTable.innerHTML += renderRow(expense);
+  $expTable.innerHTML += renderRow(expense);
   total += expense.amount;
-  renderTotal();
+  $expTotal.textContent = total;
   clearExpenseValues();
 }
 
-var removeRow = function(evt) {
+var removeExpense = function(evt) {
   var node = evt.target;
-  if ( !node.classList.contains('remove') ) return false;
+  if ( !node.classList.contains('remove') ) {
+    return false;
+  }
   var row = node.closest('.expense');
-  var amount = row.querySelector('.expense-amount').textContent;
+  // get amount for that row
+  var amount = parseInt(row.querySelector('.expense-amount').textContent);
   row.remove();
   total -= amount;
-  renderTotal();
+  $expTotal.textContent = total;
 }
 
-btn.addEventListener('click', handleClick);
-expTable.addEventListener('click', removeRow);
+var sortByName = function(evt) {
+  var $expenses = document.querySelectorAll('.expense');
+  // convert nodeList to an Array so we can call .sort
+  Array.from($expenses).sort(function(a, b) {
+    var a = a.querySelector('.expense-name').textContent;
+    var b = b.querySelector('.expense-name').textContent;
+    return a > b;
+  }).forEach(function($exp) {
+    // append sorted nodes in order
+    $expTable.append($exp);
+  });
+}
+
+var sortByAmount = function(evt) {
+  var $expenses = document.querySelectorAll('.expense');
+  // convert nodeList to an Array so we can call .sort
+  Array.from($expenses).sort(function(a, b) {
+    var a = parseInt(a.querySelector('.expense-amount').textContent);
+    var b = parseInt(b.querySelector('.expense-amount').textContent);
+    return a > b;
+  }).forEach(function($exp) {
+    // append sorted nodes in order
+    $expTable.append($exp);
+  });
+}
+
+$headingName.addEventListener('click', sortByName);
+$headingAmount.addEventListener('click', sortByAmount);
+$addExpenseBtn.addEventListener('click', addExpense);
+$expTable.addEventListener('click', removeExpense);
